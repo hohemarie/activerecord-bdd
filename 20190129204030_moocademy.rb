9146ddb3a8a69fa046b260529218f4b2ae046dbd
marie goudon <marie.goudon@laposte.net>
2019-01-30 22:22:36 +0100

    ajout de deux lignes au fichier

--- 20190129204030_moocademy.rb
+++ 20190129204030_moocademy.rb
@@ -1,6 +1,7 @@
 class moocademy < ActiveRecord::Migration[5.2]
   def change
     create_table :cours do |t|
+      t.has_many :lecons, index: true
       t.string :titre
       t.string :description
     end
@@ -10,6 +11,7 @@ class moocademy < ActiveRecord::Migration[5.2]
     #comment faire pour que la table books par ex. ait une information en plus : par ex. celle de la référence de l'auteur de chaque livre
     #rajouter la ligne t.belongs_to :author, index: true
     create_table :lecons do |t|
+      t.belongs_to :cours, index: true
       t.string :titre
       t.text :body
     end
