9a5954893e0211eea90da3c64a80be9a705c46e7
marie goudon <marie.goudon@laposte.net>
2019-01-30 22:38:25 +0100

    ajout dun fichier

--- 20190129212430_class.rb
+++ 20190129212430_class.rb
@@ -1,14 +1,15 @@
 class class < ActiveRecord::Migration[5.2]
   def change
     create_table :students do |t| 
+      t.belongs_to :class, index: true
       t.string :name
     end
     
-    create_table :class do |t| 
+    create_table :class do |t|
+      t.has_many :students, index: true
       t.string :title
     end
 
-    #TO-DO:
-    #faire les liens entre les models : dans ce site il y aura des élèves qui peuvent s'inscrire à un seul cours ; un cours pourra avoir plusieurs élèves. 
+    #liens entre les models : dans ce site il y aura des élèves qui peuvent s'inscrire à un seul cours ; un cours pourra avoir plusieurs élèves. 
   end
 end
