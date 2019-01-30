61223bf49ed06a52c0cc107c72e52e2d39181b77
marie goudon <marie.goudon@laposte.net>
2019-01-30 22:29:55 +0100

    ajout de qq lignes ds le fichie

--- 20190129204730_pinterest.rb
+++ 20190129204730_pinterest.rb
@@ -1,16 +1,21 @@
 class pinterest < ActiveRecord::Migration[5.2]
   def change
     create_table :pins do |t|
+      t.has_many :comment, index:true
       t.string :url
       t.string :users
     end
     
+    create_table :users do |t|
+      t.has_many :comment, index: true
+    end
+ 
     create_table :comment do |t|
-      t.string :users
+      t.belongs_to :pins, index: true
+      t.belongs_to :users, index: true
       t.text :body
     end
-    ##TO-DO:
-    #faire les liens entre les models : les utilisateurs peuvent commenter les pins mais ne peuvent pas commenter les commentaires
+    #liens entre les models : les utilisateurs peuvent commenter les pins mais ne peuvent pas commenter les commentaires
 
   end
 
